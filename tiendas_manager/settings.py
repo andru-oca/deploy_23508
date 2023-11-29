076122b96029ecfedd07911ac3b8fb409e37bc86
andru-oca <112781394+andru-oca@users.noreply.github.com>
2023-11-29 15:00:42 +0000

    mysql config added

--- tiendas_manager/settings.py
+++ tiendas_manager/settings.py
@@ -86,12 +86,15 @@ WSGI_APPLICATION = "tiendas_manager.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'anderson23508$tienda_manager',
+            'USER': 'anderson23508',
+            'PASSWORD': 'pirulo123',
+            'HOST': 'anderson23508.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
